a7f52d2e0bec3cabfb480bed3bade522f70e9221
Néstor Iriondo <nestoririondo@gmail.com>
2024-02-08 15:14:20 +0100

    email validator

--- models/Student.js
+++ models/Student.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
+import validator from 'validator';
 
 const StudentSchema = new mongoose.Schema({
   first_name: { required: true, type: String },
   last_name: { required: true, type: String },
-  email: { required: true, type: String },
+  email: { required: true, type: String, validate: [validator.isEmail, 'invalid email']},
 });
+
 
 const Student = mongoose.model("Student", StudentSchema)
 // if I dont add a 3rd parameter, it will create a collection called plural of model
